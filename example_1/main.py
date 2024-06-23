adbf635428bef75902cfc16bbe118962e37625b7
Shane-Stevenson <Shanestevenson04@gmail.com>
2024-06-22 17:54:46 -0700

    initial commit

---
+++ example_1/main.py
@@ -0,0 +1,25 @@
+print("hello docker")
+
+# from herbie import Herbie
+# import pandas as pd
+# import xarray as xr
+
+# H = Herbie(
+#         f"2024-06-10 00:00",
+#         model="rtma",
+#         product="anl",
+#     )
+
+# H.download(verbose=True)
+
+# ds = H.xarray("TMP:2 m")
+
+# print(ds['t2m'].data[495][480])
+
+def test():
+    if 1+1 == 2:
+        print("success")
+    else:
+        print("failure")
+
+test()
